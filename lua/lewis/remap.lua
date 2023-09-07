7f1c46fcf6ca955e124ed9bf3e608a673be16285
Lewis Sneddon <lewis.sneddon@icloud.com>
2023-09-07 20:36:14 +0100

    set some key remappings

--- lua/lewis/remap.lua
+++ lua/lewis/remap.lua
@@ -1,6 +1,26 @@
 vim.g.mapleader = " "
+-- NetRW Explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move things highlighted in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- half page up and down with centered cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- paste over but keep the original yanked to paste again
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- remap ctrl-c to escape
+vim.keymap.set("n", "<C-c>", "<Esc>")
+
+-- don't let me accidentally press 'Q' ever
+vim.keymap.set("n", "Q", "<nop>")
 
